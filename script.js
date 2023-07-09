d96ba0ae5494e2c78c5021c79197f637a18cfc95
aritradey-CS <aritra1999.dey@gmail.com>
2023-07-09 12:33:23 +0530

    removing past code and starting from trash

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-(function() {
-    let screen = document.querySelector('.screen');
-    let buttons = document.querySelectorAll('.btn-grey');
-    let clear = document.querySelector('.btn-clear');
-    let equal = document.querySelector('.btn-equal');
+// (function() {
+//     let screen = document.querySelector('.screen');
+//     let buttons = document.querySelectorAll('.btn-grey');
+//     let clear = document.querySelector('.btn-clear');
+//     let equal = document.querySelector('.btn-equal');
   
-    buttons.forEach(function(button) {
-      button.addEventListener('click', function(e) {
-        let value = e.target.textContent;
-        screen.value += value;
-      });
-    });
+//     buttons.forEach(function(button) {
+//       button.addEventListener('click', function(e) {
+//         let value = e.target.textContent;
+//         screen.value += value;
+//       });
+//     });
   
-    equal.addEventListener('click', function(e) {
-      if (screen.value === '') {
-        screen.value = 'Please enter a valid expression';
-      } else {
-        try {
-          let answer = eval(screen.value);
-          screen.value = answer;
-        } catch (error) {
-          screen.value = 'Error';
-        }
-      }
-    });
+//     equal.addEventListener('click', function(e) {
+//       if (screen.value === '') {
+//         screen.value = 'Please enter a valid expression';
+//       } else {
+//         try {
+//           let answer = eval(screen.value);
+//           screen.value = answer;
+//         } catch (error) {
+//           screen.value = 'Error';
+//         }
+//       }
+//     });
   
-    clear.addEventListener('click', function(e) {
-      screen.value = '';
-    });
-  })();
+//     clear.addEventListener('click', function(e) {
+//       screen.value = '';
+//     });
+//   })();
   
